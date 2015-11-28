d082dfc25d4734ac0f2f7c27dda35ecfbd0c650b
Florian Dumazet <floduma2@hotmail.fr>
2015-11-28 15:22:51 +0100

    Parser fichier iCal contenant des VEVENT
    J'ai essayé d'adapter le parser du tp8 au format iCal, je pense avoir
    respecté la syntaxe et avoir bien remplacé pour tout ce dont on a
    besoin, à voir

--- iCal_Parser.js
+++ iCal_Parser.js
@@ -5,53 +5,33 @@ var myArgs = process.argv.slice(2);
 var fileToParse = myArgs[0];
 
 
-fs.readFile(fileToParse, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
+fs.readFile(fileToParse, 'utf8', function (err,data) 
+{
+  if (err) 
+    return console.log(err);  
   
-  
-  analyzer = new VpfParser();
+  analyzer = new veventParser();
   analyzer.parse(data);
-  console.log("fileToParse is a valid vpf file");
-  console.log(analyzer.parsedPOI);
-  
-  for(var idx = 0; idx < analyzer.parsedPOI.length; idx++){
-	console.log(analyzer.parsedPOI[idx].name+": "+analyzer.parsedPOI[idx].averageRatings());
-  }
-
+  console.log("fileToParse is a valid iCal file");
+  console.log(analyzer.parsedvevent);
 });
 
-// POI
 
-var POI = function(nm, lt, lg, r){
-	this.name = nm;
-	this.lat = lt;
-	this.lng = lg;
-	this.ratings = r;
+// vevent var
+var vevent = function(dtstart, dtend, summary, description, location)
+{
+	this.dtstart = dtstart;
+	this.dten = dtend;
+	this.summary = summary;
+	this.description = description;
+	this.location = location;
 }
-	
-POI.prototype.averageRatings = function(){
-		if(this.ratings.length > 0){
-			var m = 0.0;
-			for(var idx = 0; idx < this.ratings.length; idx++){
-				m += parseInt(this.ratings[idx]);
-			}
-			return (m / this.ratings.length);
-		}
-		return 0;
-	};
-	
-POI.prototype.addRating = function(rating){
-		this.ratings.push(rating);
-	};
 
-
-// VpfParser
-
-var VpfParser = function(){
-	// The list of POI parsed from the input file.
-	this.parsedPOI = []
+// vevent parser
+var veventParser = function()
+{
+	// The list of iCal parsed from the input file.
+	this.parsedvevent = []
 	this.symb = ["BEGIN", "VERSION", "PRODID", "BEGIN", "DTSTART", 
 				"DTEND", "SUMMARY", "DESCRIPTION", "LOCATION", "END", "END"];
 
@@ -62,140 +42,169 @@ var VpfParser = function(){
 
 // tokenize : tranform the data input into a list
 // <eol> = CRLF
-VpfParser.prototype.tokenize = function(data){
+VpfParser.prototype.tokenize = function(data)
+{
 	//return data.split(/(\r\n|: )/);
-	var separator = /(\r\n|: )/;
+	var separator = /(\r\n|:)/;
 	data = data.split(separator);
-	data = data.filter(function(val, idx){ 
-						return !val.match(separator); 	
-					});
+	data = data.filter(function(val, idx){ return !val.match(separator);});
 	return data;
 }
 
 // parse : analyze data by calling the first non terminal rule of the grammar
-VpfParser.prototype.parse = function(data){
+veventParser.prototype.parse = function(data)
+{
 	var tData = this.tokenize(data);
 	console.log(tData);
-	this.listPoi(tData);
+	this.listvevent(tData);
 }
 
 // Parser operand
 
-VpfParser.prototype.err = function(msg, input){
-	console.log("Parsing Error ! on "+input[0]+" -- msg : "+msg);
+veventParser.prototype.err = function(msg, input)
+{
+	console.log("Parsing Error ! on " + input[0] + " -- msg : " + msg);
 	process.exit(0);
 }
 
 // Read and return a symbol from input
-VpfParser.prototype.next = function(input){
+veventParser.prototype.next = function(input)
+{
 	var curS = input.shift();
 	//console.log(curS);
 	return curS
 }
 
 // accept : verify if the arg s is part of the language symbols.
-VpfParser.prototype.accept = function(s){
+veventParser.prototype.accept = function(s)
+{
 	var idx = this.symb.indexOf(s);
 	// index 0 exists
-	if(idx === -1){
-		this.err("symbol "+s+" unknown", [" "]);
+	if(idx === -1)
+	{
+		this.err("symbol " + s + " unknown", [" "]);
 		return false;
 	}
-
 	return idx;
 }
 
 
-
 // check : check whether the arg elt is on the head of the list
-VpfParser.prototype.check = function(s, input){
-	if(this.accept(input[0]) == this.accept(s)){
+veventParser.prototype.check = function(s, input)
+{
+	if(this.accept(input[0]) == this.accept(s))
 		return true;	
-	}
 	return false;
 }
 
 // expect : expect the next symbol to be s.
-VpfParser.prototype.expect = function(s, input){
-	if(s == this.next(input)){
-		console.log("Reckognized! "+s)
+veventParser.prototype.expect = function(s, input)
+{
+	if(s == this.next(input))
+	{
+		console.log("Reckognized! " + s)
 		return true;
-	}else{
-		this.err("symbol "+s+" doesn't match", input);
-	}
+	}else
+		this.err("symbol " + s + " doesn't match", input);
 	return false;
 }
 
 
 // Parser rules
 
-// <liste_poi> = *(<poi>) "$$"
-VpfParser.prototype.listPoi = function(input){
-	this.poi(input);
+// <liste_vevent> = *(<vevent>) "$$"
+veventParser.prototype.listvevent = function(input)
+{
+	this.vevent(input);
 	this.expect("$$", input);
 }
 
-// <poi> = "START_POI" <eol> <body> <eol> "END_POI"
-VpfParser.prototype.poi = function(input){
+// <vevent> = "BEGIN" <eol> <body> <eol> "END"
+veventParser.prototype.vevent = function(input)
+{
 
-	if(this.check("START_POI", input)){
-		this.expect("START_POI", input);
+	if(this.check("BEGIN", input))
+	{
+		this.expect("BEGIN", input);
 		var args = this.body(input);
-		var p = new POI(args.nm, args.lt, args.lg, []);
+		var p = new iCal(args.dtstart, args.dtend, args.summary, args.description, args.location, []);
 		this.note(input, p);
-		this.expect("END_POI",input);
-		this.parsedPOI.push(p);
-		this.poi(input);
+		this.expect("BEGIN",input);
+		this.parsedvevent.push(p);
+		this.vevent(input);
 		return true;
-	}else{
+	}else
 		return false;
-	}
 
 }
 
-// <body> = <name> <eol> <latlng> <eol> <optional>
-VpfParser.prototype.body = function(input){
-	var nm = this.name(input);
-	var ltlg = this.latlng(input);
-	return { nm: nm, lt: ltlg.lat, lg: ltlg.lng };
+// <body> = <dtstart> <eol> <dtend> <eol> <summary> <eol> <description> <eol> <location>
+veventParser.prototype.body = function(input)
+{
+
+	var dtstart = this.dtstart(input);
+	var dtend = this.dtend(input);
+	var summary = this.summary(input);
+	var description = this.description(input);
+	var location = this.location(input);
+	return { dtstart: dtstart, dtend: dtend, summary: summary, description: description, location: location };
 }
 
-// <name> = "name: " 1*WCHAR
-VpfParser.prototype.name = function(input){
-	this.expect("name",input)
+// <dtstart> = "DTSTART:" 8DIGIT "T" 6DIGIT "Z"
+veventParser.prototype.dtstart = function(input)
+{
+	this.expect("DTSTART",input)
 	var curS = this.next(input);
-	if(matched = curS.match(/[\wàéèêîù'\s]+/i)){
+	if(matched = curS.match(/(\d{8})T(\d{6})Z/))
 		return matched[0];
-	}else{
-		this.err("Invalid name", input);
-	}
+	else
+		this.err("Invalid dtstart", input);
 }
 
-// <latlng> = "latlng: " 1*3DIGIT "." 1*DIGIT", " 1*3DIGIT "." 1*DIGIT
-VpfParser.prototype.latlng = function(input){
-	this.expect("latlng",input)
+// <dtend> = "DTEND:" 8DIGIT "T" 6DIGIT "Z"
+veventParser.prototype.dtend = function(input)
+{
+	this.expect("DTEND",input)
 	var curS = this.next(input);
-	if(matched = curS.match(/(\d+(\.\d+)?);(\d+(\.\d+)?)/)){
-		return { lat: matched[1], lng: matched[3] };
-	}else{
-		this.err("Invalid latlng", input);
-	}
+	if(matched = curS.match(/(\d{8})T(\d{6})Z/))
+		return matched[0];
+	else
+		this.err("Invalid dtend", input);
 }
 
-// <optional> = *(<note>)
-// <note> = "note: " 1DIGIT
-VpfParser.prototype.note = function (input, curPoi){
-	if(this.check("note", input)){
-		this.expect("note", input);
-		var curS = this.next(input);
-		if(matched = curS.match(/\d/)){
-			curPoi.addRating(matched[0]);
-			this.note(input, curPoi);
-		}else{
-			this.err("Invalid note");
-		}	
-	}
+// <summary> = "SUMMARY:" 1*(WSP/VCHAR)
+veventParser.prototype.summary = function(input)
+{
+	this.expect("SUMMARY",input)
+	var curS = this.next(input);
+	if(matched = curS.match(/(\w)*/))
+		return matched[0];
+	else
+		this.err("Invalid summary", input);
 }
 
-exports.POI = POI;
-exports.VpfParser = VpfParser;
+// <description> = "DESCRIPTION:" 1*(WSP/VCHAR)
+veventParser.prototype.description = function(input)
+{
+	this.expect("DESCRIPTION",input)
+	var curS = this.next(input);
+	if(matched = curS.match(/(\w)*/))
+		return matched[0];
+	else
+		this.err("Invalid description", input);
+}
+
+// <location> = "LOCATION:" 1*(WSP/VCHAR)
+veventParser.prototype.description = function(input)
+{
+	this.expect("LOCATION",input)
+	var curS = this.next(input);
+	if(matched = curS.match(/(\w)*/))
+		return matched[0];
+	else
+		this.err("Invalid location", input);
+}
+
+
+exports.vevent = Vevent;
+exports.veventParser = veventParser;
