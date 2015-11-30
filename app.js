9dd4e0b2a03af5b0c9133ba2e9de41461394d45a
PunKeel <punkeel@me.com>
2015-11-30 18:34:10 +0100

    Add parser (wip)

---
+++ app.js
@@ -0,0 +1,9 @@
+if(require.main === module){
+	// We're running as a cli app
+	var csv = require('./lib/csv');
+	console.log(csv.parseFile(__dirname + '/test/data/sample.csv'));
+}else{
+	// We're being used as a library !
+	// Then we should expose our API
+	// module.exports = ...;
+}
