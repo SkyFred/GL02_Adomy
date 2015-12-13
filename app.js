660e45c08f0bc94a5bcc95cebf8651a887b719eb
Hugo Marcellin <marcellinhugo@orange.fr>
2015-12-13 22:24:32 +0100

    Version finale, correction fonctions complémentaire et volume horaire dan emploi_du_temps.js

--- app.js
+++ app.js
@@ -2,28 +2,9 @@
 
 if(require.main === module){
 	// We're running as a cli app
-	
-	/*
 	var csv = require('./lib/csv');
-	var edt = csv.parseFile(__dirname + '/test/data/sample.csv');*/
-
-
-
 	var ical = require('./lib/ical_Parser');
-	var edt = ical.parseFile(__dirname + '/test/data/sample.ics');
-	
-	edt.exportCsv();
-
-/*
-    var EmploiDuTemps = require('./lib/emploi_du_temps');
-	var edt = new EmploiDuTemps("Michel");
-
-	edt.ajouter(0,46,5,"Mark","Jardiland");
-	edt.ajouter(1,0,3,null,"UTT");
-
-
-	var data = edt.exportiCal();*/
-
+	var EmploiDuTemps = require('./lib/emploi_du_temps');
 
 
 
